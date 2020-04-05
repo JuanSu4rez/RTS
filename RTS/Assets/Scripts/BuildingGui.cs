e7426aceeb2f0eb99e0f69523c5e0e2596d36f72
DESKTOP-QH7A189\juan <jnssuarez@gmnail.com>
2020-04-05 18:01:59 +1200

    working version, empty button removed

--- RTS/Assets/Scripts/BuildingGui.cs
+++ RTS/Assets/Scripts/BuildingGui.cs
@@ -25,7 +25,7 @@ public class BuildingGui : ScriptableObject, IGui
         if (behavior == null)
             return;
 
-        GUI.Button(CameraScript.gameareas.GuiArea, "");
+        //GUI.Button(CameraScript.gameareas.GuiArea, "");
 
         gameFacade = GameScript.GetFacade(behavior.Team);
  
