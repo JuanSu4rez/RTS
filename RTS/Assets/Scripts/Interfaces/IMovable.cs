bf1b1da130a2030bbf5f4ca8e37817903b6ce944
Andres Vargas <archarry>
2023-04-08 00:38:19 -0500

    Deleting old stuff

--- RTS/Assets/Scripts/Interfaces/IMovable.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEditor;
-using System;
-
-public interface IMovable {
-
-    void Move(Vector3 position, Action action = null);//, Action releaseaction= null);
-
-}
--- RTS/Assets/Scripts/Interfaces/ISelectable.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using UnityEditor;
-using System;
-public interface ISelectable {
-    bool IsSelected { get; set; }
-
-}
